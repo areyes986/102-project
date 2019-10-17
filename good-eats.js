b98f66364e6d4d5863960ed9551263f5eabfcd8a
Allyson Reyes <areyes986@gmail.com>
2019-10-16 21:07:34 -0700

    adding js

---
+++ good-eats.js
@@ -0,0 +1,10 @@
+var answer = prompt('Are you ready for some good food?!?');
+var message;
+
+if (answer === 'yes') {
+    message = 'dis food broke da mout!';
+} else if (answer === 'no') {
+    message = 'you so dakine liddat';
+}
+
+document.write (`<h3>` + message + '<h3>')
